daca3aa6407bb2b526f7e466b03994c4329ba890
Ruben van der Ham <ravdham@protonmail.com>
2017-11-15 15:59:11 +0100

    Finished 2.4a-b

--- Assignment 2/Excersises/24a.R
+++ Assignment 2/Excersises/24a.R
@@ -1,9 +1,24 @@
 source("function2.txt")
 par(mfrow=c(1,1))
 maximaOfDice = maxdice(1000,2)
-
+meanArray = c()
 for(i in 1:length(maximaOfDice)){
   meanArray[[i]]= mean(maximaOfDice[1:i])
 }
 names(meanArray) = c(1:length(meanArray))
-plot(meanArray,names.arg =names(meanArray),type = "l",ylab = "Mean of the maximum sum(s) of 2 dice",xlab = "Amount of throws with 2 dice", main = "Mean of max sum with N throws of 2 dice",ylim = c(2,5))
+plot(meanArray,names.arg =names(meanArray),type = "l",ylab = "Mean of the maximum of 2 dice",xlab = "Amount of throws with 2 dice", main = "Mean of max on 2 dice with N throws ",ylim = c(1,6))
+
+# Expectation
+probability = c()
+randomVar = c()
+uniques = unique(maximaOfDice,incomparables = FALSE)
+
+for(i in sort(uniques)){
+  probability = c(probability,length(which(maximaOfDice == i))/length(maximaOfDice))
+  randomVar = c(randomVar,i)
+}
+
+expectation = c(randomVar*probability)
+table = cbind(randomVar,probability,expectation)
+sum(expectation)
+table
--- Assignment 2/Excersises/24b.R
+++ Assignment 2/Excersises/24b.R
@@ -12,5 +12,22 @@ for(i in sort(uniques)){
 
 expectation = c(randomVar*probability)
 cbind(randomVar,probability,expectation)
-#answer 1:
 sum(expectation)
+
+
+#second part of B
+maximaOf2Dice = maxdice(1000000,2)
+probability = c()
+randomVar = c()
+uniques = unique(maximaOf2Dice,incomparables = FALSE)
+
+for(i in sort(uniques)){
+  probability = c(probability,length(which(maximaOf2Dice == i))/length(maximaOf2Dice))
+  randomVar = c(randomVar,i)
+}
+
+expectation = c(randomVar*probability)
+cbind(randomVar,probability)
+sum(probability)
+probability[[3]]
+
